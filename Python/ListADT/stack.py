2191879d5862c29c00ec395ee501878b15c9a299
Sahil Shah <sahil.p.shah5@gmail.com>
2018-09-08 20:25:19 -0500

    stack

---
+++ Python/ListADT/stack.py
@@ -0,0 +1,16 @@
+class Stack:
+    def __init__(self):
+        stack = []
+
+    def push(self, data):
+        self.stack.append(data)
+    def pop(self):
+        return self.stack.pop(0)
+    def peek(self):
+        return self.stack[0]
+    def size(self):
+        return len(self.stack)
+    def isEmpty(self):
+        if self.size() != 0:
+            return False
+        return True
