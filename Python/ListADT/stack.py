1398f7de4b8525182f83086138914c3730449de6
Sahil Shah <sahil.p.shah5@gmail.com>
2018-09-08 20:53:06 -0500

    spacing

--- Python/ListADT/stack.py
+++ Python/ListADT/stack.py
@@ -4,12 +4,16 @@ class Stack:
 
     def push(self, data):
         self.stack.append(data)
+
     def pop(self):
         return self.stack.pop(0)
+
     def peek(self):
         return self.stack[0]
+
     def size(self):
         return len(self.stack)
+    
     def isEmpty(self):
         if self.size() != 0:
             return False
